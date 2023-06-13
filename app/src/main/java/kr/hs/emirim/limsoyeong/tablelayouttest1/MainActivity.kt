a6ad18ea46da6d0d1fac7b06421891df2d947efd
thdud__ <love05061114@gmail.com>
2023-06-13 19:54:21 +0900

    TableLayout을 사용한 계산기(키패드 있음)

--- app/src/main/java/kr/hs/emirim/limsoyeong/tablelayouttest1/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/limsoyeong/tablelayouttest1/MainActivity.kt
@@ -2,8 +2,11 @@ package kr.hs.emirim.limsoyeong.tablelayouttest1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit internal var edit1 : EditText
@@ -12,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     lateinit internal var btnMinus : Button
     lateinit internal var btnMultiply : Button
     lateinit internal var btnDivide : Button
+    lateinit internal var textResult : TextView
     lateinit var num1 : String
     lateinit var num2 : String
     var result : Int? = null
@@ -29,5 +33,46 @@ class MainActivity : AppCompatActivity() {
         btnMultiply = findViewById(R.id.btnMultiply)
         btnDivide = findViewById(R.id.btnDivide)
         textResult = findViewById(R.id.textResult)
+        btnPlus.setOnClickListener(btnListener)
+        btnMinus.setOnClickListener(btnListener)
+        btnMultiply.setOnClickListener(btnListener)
+        btnDivide.setOnClickListener(btnListener)
+
+        for(i in 0..btnIds.size-1 step 1){
+            btns.add(findViewById(btnIds[i]))
+        }
+
+        for(i in 0..btnIds.size-1 step 1){
+            btns[i].setOnClickListener {
+                if(edit1.isFocused){
+                    num1 = edit1.text.toString() + btns[i].text.toString()
+                    edit1.setText(num1)
+                }else if(edit2.isFocused){
+                    num2 = edit2.text.toString() + btns[i].text.toString()
+                    edit2.setText(num2)
+                }else{
+                    Toast.makeText(applicationContext, "먼저 입력할 곳에 포커스를 주세요", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+        }
     }
+
+    var btnListener = OnClickListener {
+        num1 = edit1.text.toString()
+        num2 = edit2.text.toString()
+
+        when(it.id){
+            R.id.btnPlus -> result = num1.toInt() + num2.toInt()
+            R.id.btnMinus -> result = num1.toInt() - num2.toInt()
+            R.id.btnMultiply -> result = num1.toInt() * num2.toInt()
+            R.id.btnDivide -> result = num1.toInt() / num2.toInt()
+
+        }
+        textResult.setText(R.string.text_result)
+        textResult.append(result.toString())
+        textResult.text = "계산결과 : " + result
+    }
+
 }
